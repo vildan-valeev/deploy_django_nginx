663c3dd3334704aeb283bb892dd140451bcfde1c
vildan-valeev <vildan.maydanovich.valeev@gmail.com>
2021-09-02 01:25:23 +0500

    refactoring nginx config

--- backend_repo/rand/main.py
+++ backend_repo/rand/main.py
@@ -8,16 +8,17 @@ routes = web.RouteTableDef()
 @routes.get('/rand/')
 async def handle(request: web.Request) -> web.Response:
     """Получаем апдейты"""
-    r_int = random.randint(100, 200)
-
-    return web.Response(status=200, text=f'<h1>{r_int}</h1>',
-                        content_type='text/html')  # status=200
+    r = random.randint(100, 200)
+    text = f'<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Title</title></head><body><h1>{r}</h1' \
+           f'></body></html>'
+    # text = f'<h1>{r}</h1>'
+    return web.Response(status=200, text=text,
+                        content_type='text/html', )  # status=200
 
 
 @routes.get('/tik/')
 async def handle(request: web.Request) -> web.Response:
     """Получаем апдейты"""
-
     return web.Response(status=200, text=f'tik',
                         )  # status=200
 
